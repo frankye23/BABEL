beb1f297bb2dba0d0eb8a7f6593174a64c09d0b5
frankye23 <dafeng.ye@intchains.com>
2021-10-25 09:34:54 +0800

    初始化

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+import Home from "../views/Home.vue";
+
+const routes = [
+  { path: "/", name: "Home", component: Home },
+  {
+    path: "/about",
+    name: "About",
+    component: () => import("../views/About.vue"),
+  },
+];
+
+export default createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
---
+++ src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <n-button>关于</n-button>
+</template>
+
+<script>
+  import { defineComponent } from 'vue'
+  import { NButton } from 'naive-ui'
+
+  export default defineComponent({
+    components: {
+      NButton
+    }
+  })
+</script>
